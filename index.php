dfd51099cfe66615312cdf9373720daf52585286
arshad031 <arshad.cse.sub48@gmail.com>
2025-07-02 23:56:34 +0600

    Initial commit for photo-gallary project

--- index.php
+++ index.php
@@ -7,22 +7,26 @@ $sql = "SELECT * FROM image ORDER BY upload_date DESC";
 $stmt = $pdo->query($sql);
 $images = $stmt->fetchAll();
 ?>
-<div class="my-5-text-center">
-    <h1 class="display-4">Photo Gallary</h1>
-    <p class="lead">Latest Uploaded Image</p>
-</div>
-<div class="row">
-  <?php foreach($images as $image):?>
-  <div class="card" style="width: 18rem;">
-  <img src="assets/images/<?php echo htmlspecialchars($image['filename']);?>" class="card-img-top" alt="<?php echo htmlspecialchars($image['title']);?>">
-  <div class="card-body">
-    <h5 class="card-title"><?php echo htmlspecialchars($image['title']);?></h5>
-    <p class="text-muted small">Uploaded On: <?php echo date("F j, Y", strtotime($image['upload_date']));?>  </p>
-    <p class="card-text"> <?php echo htmlspecialchars($image['description']);?></p>
+
+<div class="container py-4">
+    <h2 class="text-center mb-4">Photo Gallery</h2>
+    <div class="row g-4">
+      <?php foreach($images as $image):?>
+      <div class="col-md-3">
+        <div class="card h-100">
+          <img src="assets/images/<?php echo htmlspecialchars($image['filename']);?>" class="card-img-top" alt="<?php echo htmlspecialchars($image['title']);?>">
+          <div class="card-body">
+            <h5 class="card-title"><?php echo htmlspecialchars($image['title']);?></h5>
+            <p class="text-muted small mb-1">Uploaded On: <?php echo date("F j, Y", strtotime($image['upload_date']));?></p>
+            <p class="card-text"><?php echo htmlspecialchars($image['description']);?></p>
+          </div>
+        </div>
+      </div>
+      <?php endforeach;?>
+
+    </div>
   </div>
-</div>
-<?php endforeach;?>
-</div>
+
 <?php
 include 'includes/footer.php';
 ?>
